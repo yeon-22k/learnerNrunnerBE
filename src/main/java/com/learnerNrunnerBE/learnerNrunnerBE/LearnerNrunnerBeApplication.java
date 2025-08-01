dcdecb5e82debe00a7fd67ae67d1407ffdce1454
yeon-22k <pink5465y@gmail.com>
2025-08-01 16:50:30 +0900

    initial commit

---
+++ src/main/java/com/learnerNrunnerBE/learnerNrunnerBE/LearnerNrunnerBeApplication.java
@@ -0,0 +1,13 @@
+package com.learnerNrunnerBE.learnerNrunnerBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnerNrunnerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnerNrunnerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnerNrunnerBE/learnerNrunnerBE/LearnerNrunnerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnerNrunnerBE.learnerNrunnerBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnerNrunnerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
